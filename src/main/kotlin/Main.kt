c9d034856df4a0387d7ddf527a3bde88c4f8502c
Sehyru <yakovenko1994@gmail.com>
2026-05-21 22:07:15 +0300

    KS-3-1: string greeting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
 fun main() {
+    val userName = "Сергей"
 
+    var greetings = "Добрый день, $userName!"
+
+    println(greetings)
+
+    greetings = "Добрый вечер, $userName!"
+
+    println(greetings)
 }
