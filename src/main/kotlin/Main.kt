1f5e2205f594a1bd31d4f6bb572590e000e5396f
Sergey Yakovenko <61623375+Sehyru@users.noreply.github.com>
2026-05-22 18:44:50 +0300

    Revert "KS-3-3: string templates"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,5 @@
 package org.example
 
 fun main() {
-    val number = 9
-    println("""$number * 1 = ${1 * number} 
-              |$number * 2 = ${2 * number} 
-              |$number * 3 = ${3 * number} 
-              |$number * 4 = ${4 * number} 
-              |$number * 5 = ${5 * number} 
-              |$number * 6 = ${6 * number} 
-              |$number * 7 = ${7 * number} 
-              |$number * 8 = ${8 * number} 
-              |$number * 9 = ${9 * number}""".trimMargin())
+
 }
