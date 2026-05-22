93b29b82786070599d55f86f7ea41fd761450fc7
Sehyru <yakovenko1994@gmail.com>
2026-05-23 00:42:22 +0300

    KS-3-4: string methods, edits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,12 @@ fun main() {
     var moveFrom = "E2"
     var moveWhere = "E4"
     var numberMove = 1
-    var move = "$moveFrom-$moveWhere, $numberMove"
+    var move = "$moveFrom-$moveWhere;$numberMove"
     println(move)
 
     moveFrom = "D2"
-    moveWhere = "D1"
-    numberMove = 2
-    move = "$moveFrom-$moveWhere, $numberMove"
+    moveWhere = "D3"
+    numberMove++
+    move = "$moveFrom-$moveWhere;$numberMove"
     println(move)
 }
