cb9c2092b2d2a2028f00e336b5a126be704f95af
Sehyru <yakovenko1994@gmail.com>
2026-05-23 19:44:09 +0300

    KS-3-5: fix split parser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 package org.example
 
 fun main() {
-    var move = "D2-D4;0"
-    var split = move.split("-", ";")
-    var moveFrom = split[0]
-    var moveWhere = split[1]
-    var moveNumber = split[2]
+    val move = "D2-D4;0"
+    val split = move.split("-", ";")
+    val moveFrom = split[0]
+    val moveWhere = split[1]
+    val moveNumber = split[2]
 
     println(moveFrom)
     println(moveWhere)
