2bdc5695128053bc12686d02a6a957ef94cb910c
Sehyru <yakovenko1994@gmail.com>
2026-05-24 22:20:39 +0300

    KS-4-1: fix boolean comparison - table reservations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,8 @@ fun main() {
     val bookedTablesToday = 13
     val bookedTablesTomorrow = 9
 
-    val isAvailableToday: Boolean = bookedTablesToday < TOTAL_TABLES
-    val isAvailableTomorrow: Boolean = bookedTablesTomorrow < TOTAL_TABLES
+    val isAvailableToday = bookedTablesToday < TOTAL_TABLES
+    val isAvailableTomorrow = bookedTablesTomorrow < TOTAL_TABLES
 
     println("[Доступность столиков на сегодня: $isAvailableToday], \n[Доступность столиков на завтра: $isAvailableTomorrow]")
 
