a356b480a1157297397a68b4a54484ce27dfd3ed
Sehyru <yakovenko1994@gmail.com>
2026-05-24 22:13:05 +0300

    KS-4-1: Comparison - Table Reservations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
-
+const val TOTAL_TABLES = 13
 fun main() {
+
+    val bookedTablesToday = 13
+    val bookedTablesTomorrow = 9
+
+    val isAvailableToday: Boolean = bookedTablesToday < TOTAL_TABLES
+    val isAvailableTomorrow: Boolean = bookedTablesTomorrow < TOTAL_TABLES
+
+    println("Доступность столиков на сегодня: $isAvailableToday \nДоступность столиков на завтра: $isAvailableTomorrow")
 
 }
