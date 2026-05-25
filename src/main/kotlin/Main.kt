5985428938d5f404bdafb8946729a09fba06f96c
Sehyru <yakovenko1994@gmail.com>
2026-05-25 17:55:09 +0300

    KS-4-2: equality - customs terminal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package org.example
 
 fun main() {
+    val minWeight = 35
+    val maxWeight = 100
+    val maxVolume = 100
 
+    var weight = 20
+    var volume = 80
+
+    println("Груз с весом $weight кг и объёмом $volume л соответствует категории 'Average': ${(weight > minWeight) && (weight <= maxWeight) && (volume < maxVolume)}")
+
+    weight = 50
+    volume = 100
+
+    println("Груз с весом $weight кг и объёмом $volume л соответствует категории 'Average': ${(weight > minWeight) && (weight <= maxWeight) && (volume < maxVolume)}")
 }
