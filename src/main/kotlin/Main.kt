0eeab4833c18d527fd7c11969e5777b23d25b532
Sehyru <yakovenko1994@gmail.com>
2026-05-25 18:01:35 +0300

    KS-4-2: fix equality - customs terminal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,24 @@
 package org.example
 
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUME_MAX = 100
+
 fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
 
     var weight = 20
     var volume = 80
 
-    println("Груз с весом $weight кг и объёмом $volume л соответствует категории 'Average': ${(weight > minWeight) && (weight <= maxWeight) && (volume < maxVolume)}")
+    println(
+        "Груз с весом $weight кг и объёмом $volume л соответствует категории 'Average': " +
+                "${(weight > WEIGHT_MIN) && (weight <= WEIGHT_MAX) && (volume < VOLUME_MAX)}"
+    )
 
     weight = 50
     volume = 100
 
-    println("Груз с весом $weight кг и объёмом $volume л соответствует категории 'Average': ${(weight > minWeight) && (weight <= maxWeight) && (volume < maxVolume)}")
+    println(
+        "Груз с весом $weight кг и объёмом $volume л соответствует категории 'Average': " +
+                "${(weight > WEIGHT_MIN) && (weight <= WEIGHT_MAX) && (volume < VOLUME_MAX)}"
+    )
 }
