4331858e088ef368b9ba59ab67aeb06011447d7a
Sehyru <yakovenko1994@gmail.com>
2026-05-26 18:17:54 +0300

    KS-4-3: fix logic && and || - plant monitoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,17 @@
 package org.example
 
-const val FAVORABLE_HUMIDITY: Int = 20
-const val FAVORABLE_SEASON: String = "зима"
+const val FAVORABLE_HUMIDITY = 20
+const val FAVORABLE_SEASON = "не зима"
 
 fun main() {
+
     val isSunny: Boolean = true
     val isTentOpen: Boolean = true
+    val currentHumidity: Int = 20
+    val currentSeason: String = "зима"
 
-    val isConditionsFavorable = isSunny && isTentOpen && (FAVORABLE_HUMIDITY == 20) && (FAVORABLE_SEASON != "зима")
+    val isConditionsFavorable =
+        isSunny && isTentOpen && (currentHumidity == FAVORABLE_HUMIDITY) && (currentSeason == FAVORABLE_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isConditionsFavorable")
 }
