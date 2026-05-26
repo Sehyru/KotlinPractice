17a23bafb9d57ab9c6e6c2feb3c32f81d91f1dcc
Sehyru <yakovenko1994@gmail.com>
2026-05-26 17:52:44 +0300

    KS-4-3: logic && and || - plant monitoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.example
 
 fun main() {
+    val isSunny: Boolean = true
+    val isTentOpen: Boolean = true
+    val humidity: Int = 20
+    val currentSeason: String = "зима"
 
+    val isConditionsFavorable = isSunny && isTentOpen && (humidity == 20) && (currentSeason != "зима")
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $isConditionsFavorable")
 }
