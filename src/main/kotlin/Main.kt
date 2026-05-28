4df43aa4de13932e4d33ee271ac3a66d957bd54e
Sehyru <yakovenko1994@gmail.com>
2026-05-28 19:13:18 +0300

    KS-5-1: add randomizer and change text confirmation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 package org.example
 
+import kotlin.random.Random
+
 fun main() {
 
-    val num1 = 24
-    val num2 = 12
+    val num1 = Random.nextInt(1, 101)
+    val num2 = Random.nextInt(1, 101)
     val result = num1 + num2
 
-    println("Сколько будет $num1 + $num2 = ?")
+    println("Подтвердите, что вы не робот, сложите два числа: $num1 и $num2")
 
     val userAnswer = readln().toIntOrNull()
 
