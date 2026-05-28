f165117de59ac6174c2ee702bc1dcfbc6623ec5b
Sehyru <yakovenko1994@gmail.com>
2026-05-28 19:24:35 +0300

    KS-5-1: fix range in randomizer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ import kotlin.random.Random
 
 fun main() {
 
-    val num1 = Random.nextInt(1, 101)
-    val num2 = Random.nextInt(1, 101)
+    val num1 = Random.nextInt(1, 100)
+    val num2 = Random.nextInt(1, 100)
     val result = num1 + num2
 
     println("Подтвердите, что вы не робот, сложите два числа: $num1 и $num2")
