9cd1e1d8a6e4501fd269306396124172fa3b4cfa
Sehyru <yakovenko1994@gmail.com>
2026-05-28 19:01:28 +0300

    KS-5-1: if - authorization check

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,13 @@ package org.example
 
 fun main() {
 
+    val num1 = 24
+    val num2 = 12
+    val result = num1 + num2
+
+    println("Сколько будет $num1 + $num2 = ?")
+
+    val userAnswer = readln().toIntOrNull()
+
+    if (userAnswer == result) println("Добро пожаловать!") else println("Доступ запрещен.")
 }
