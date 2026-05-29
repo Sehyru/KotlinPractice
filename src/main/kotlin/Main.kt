bcc11b974be20256af67f86be37d9b0cee224e68
Sehyru <yakovenko1994@gmail.com>
2026-05-29 14:10:32 +0300

    KS-5-2: fix logic current year and readln, remove additional verifications

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,12 @@
 package org.example
 
-import java.time.LocalDate
-
 const val AGE_OF_MAJORITY = 18
 
 fun main() {
 
-    val today = LocalDate.now()
-    val currentYear = today.year
-
+    val currentYear = 2026
     println("Введите ваш год рождения:")
-    val userBirthYearInput = readln().toIntOrNull()
-
-    if (userBirthYearInput == null) {
-        println("Ошибка: введите корректный год рождения.")
-        return
-    }
-
-    if (userBirthYearInput > currentYear) {
-        println("Ошибка: год рождения не может быть больше текущего $currentYear года.")
-        return
-    }
-
+    val userBirthYearInput = readln().toInt()
     val userAge = currentYear - userBirthYearInput
 
     if (userAge >= AGE_OF_MAJORITY) {
