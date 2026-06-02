a66aef05cbad69cf890bad9b2b97efdb70ad7c8d
Sehyru <yakovenko1994@gmail.com>
2026-06-02 15:49:47 +0300

    KS-6-1: while - authorization in app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,28 @@
 package org.example
 
 fun main() {
+    println("Добро пожаловать! Вы выбрали регистрацию.")
+    println("Создайте логин: ")
+    val registeredLogin = readln()
 
+    println("Создайте пароль: ")
+    val registeredPassword = readln()
+
+    println("Регистрация завершена! Теперь войдите в приложение!")
+
+    var isAuthorized = false
+
+    while (!isAuthorized) {
+        println("Введите логин: ")
+        val inputLogin = readln()
+
+        println("Введите пароль: ")
+        val inputPassword = readln()
+
+        if (inputLogin == registeredLogin && inputPassword == registeredPassword) {
+            isAuthorized = true
+            println("Авторизация прошла успешно")
+        } else
+            println("Неверный логин и пароль. Попробуйте еще раз.")
+    }
 }
