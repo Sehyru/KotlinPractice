6decc9bd79e68826c1b2bdb0877087787cb1412d
Sehyru <yakovenko1994@gmail.com>
2026-06-03 21:45:35 +0300

    KS-6-2: remove while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,7 @@ fun main() {
     println("Введите количество секунд: ")
     val seconds = readln().toInt()
 
-    var count = seconds
-    while (count > 0) {
-        Thread.sleep(1000)
-        count--
-    }
+    Thread.sleep(seconds * 1000L)
+
     println("Прошло $seconds секунд")
 }
