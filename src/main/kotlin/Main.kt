b54077c169f8b3b14f044a021b89532e9ca31af0
Sehyru <yakovenko1994@gmail.com>
2026-06-04 00:01:24 +0300

    KS-6-3: do-while - countdown timer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
 fun main() {
+    println("Введите количество секунд: ")
+    val seconds = readln().toInt()
 
+    var count = seconds
+    while (count > 0) {
+        Thread.sleep(1000L)
+        println("Осталось секунд: ${count--}")
+    }
+    println("Время вышло")
 }
