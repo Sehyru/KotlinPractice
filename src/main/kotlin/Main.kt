badb113a0341d50f876e25881b1a95da95a75cb1
Sehyru <yakovenko1994@gmail.com>
2026-06-03 21:28:53 +0300

    KS-6-2: count while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
 fun main() {
+    println("Введите количество секунд: ")
+    val seconds = readln().toInt()
+    var currentSecond = 0
 
+    while (currentSecond < seconds) {
+        Thread.sleep(1000)
+        currentSecond++
+    }
+    println("Прошло $seconds секунд")
 }
