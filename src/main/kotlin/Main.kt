e17cc4472c1295bcc625bd0cfa1a9974e125f2a4
Sehyru <yakovenko1994@gmail.com>
2026-06-06 15:59:03 +0300

    KS-6-4: break - guess number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
 package org.example
 
 fun main() {
+    val number = (1..9).random()
+    var attempt = 5
 
+    println("Добро пожаловать в игру «Угадай число»!")
+    println("Я загадал число от 1 до 9. У вас есть $attempt попыток, чтобы его угадать.")
+
+    while (attempt > 0) {
+        println("Введите ваше число: ")
+        val userNumber = readln().toInt()
+
+        if (userNumber == number) {
+            break
+        }
+
+        attempt--
+
+        if (attempt > 0) {
+            println("Неверно! Осталось попыток: $attempt")
+        }
+    }
+
+    if (attempt > 0) {
+        println("Это была великолепная игра!")
+    } else {
+        println("Было загадано $number")
+    }
 }
