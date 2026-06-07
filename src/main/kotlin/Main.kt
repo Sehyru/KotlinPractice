04015c177504d8766a96f069936fa606957c773b
Sehyru <yakovenko1994@gmail.com>
2026-06-07 16:01:39 +0300

    KS-6-5: fix logic, remove continue and success

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,29 @@
 package org.example
 
+const val MAX_ATTEMPTS = 3
+
 fun main() {
     var attempts = 0
-    var success = false
 
     println("Добро пожаловать! Пройдите проверку.")
+    println("Докажите что вы не бот. У вас есть $MAX_ATTEMPTS попытки.")
 
-    while (attempts < 3 && !success) {
+    while (attempts < MAX_ATTEMPTS) {
         val number1 = (1..9).random()
         val number2 = (1..9).random()
 
-        println("Докажите что вы не бот, сложите два числа $number1 и $number2. У вас есть ${3 - attempts} попытки.")
         println("$number1 + $number2 = ")
 
+
         val sum = readln().toInt()
-        attempts++
-
-        if (sum != number1 + number2) {
-            println("Неверно! Осталось попыток: ${3 - attempts}")
-            continue
+        if (sum == number1 + number2) {
+            println("Добро пожаловать!")
+            break
         }
-
-        success = true
+        attempts++
+        println("Неверно! Осталось попыток: ${MAX_ATTEMPTS - attempts}")
     }
-
-    if (success) {
-        println("Добро пожаловать!")
-    } else {
+    if (attempts == MAX_ATTEMPTS) {
         println("Доступ запрещен")
     }
 }
