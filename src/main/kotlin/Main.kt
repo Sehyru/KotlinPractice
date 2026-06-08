37f9b4ce9369ad7506df784574003c508fda6a9b
Sehyru <yakovenko1994@gmail.com>
2026-06-08 16:28:40 +0300

    KS-7-1: for - password generator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package org.example
 
-fun main() {
+import kotlin.random.Random
 
+fun main() {
+    val digits = "0123456789"
+    val letters = "abcdefghijklmnopqrstuvwxyz"
+
+    var password = ""
+
+    for (i in 1..6) {
+        password += if (i % 2 == 0) digits[Random.nextInt(digits.length)]
+        else letters[Random.nextInt(letters.length)]
+    }
+
+    println(password)
 }
