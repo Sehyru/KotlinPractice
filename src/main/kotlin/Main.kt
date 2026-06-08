a074cf8b02b158190e46bbe480cbf08c84dad1f9
Sehyru <yakovenko1994@gmail.com>
2026-06-08 19:42:25 +0300

    KS-7-2: upgrade logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,20 @@
 package org.example
 
 fun main() {
-    var correctCode = (1000..9999).random()
-    println("Ваш код авторизации: $correctCode")
+    var correctCode: Int
+    var inputCode: Int
 
     do {
-        print("Введите код авторизации: ")
-        val inputCode = readln().toInt()
-
-        if (inputCode == correctCode) {
-            println("Добро пожаловать!")
-            break
-        }
-
         correctCode = (1000..9999).random()
-        println("Неверный код. Ваш новый код авторизации: $correctCode")
-    } while (true)
+        println("Ваш код авторизации: $correctCode")
+
+        print("Введите код авторизации: ")
+        inputCode = readln().toInt()
+
+        if (inputCode != correctCode) {
+            println("Неверный код.")
+        }
+    } while (inputCode != correctCode)
+
+    println("Добро пожаловать!")
 }
