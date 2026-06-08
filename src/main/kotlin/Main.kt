afc7ade81e1ca6a71354961b7bc1969dc313cce8
Sehyru <yakovenko1994@gmail.com>
2026-06-08 19:20:19 +0300

    KS-7-2: downTo - SMS authorization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
 
 fun main() {
+    var correctCode = (1000..9999).random()
 
+    println("Ваш код авторизации: $correctCode")
+    for (sec in 60 downTo 1) {
+
+        println("Введите код авторизации: ")
+        val inputCode = readln().toInt()
+        if (inputCode == correctCode) {
+            println("Добро пожаловать!")
+            break
+        }
+        correctCode = (1000..9999).random()
+        println("Осталось $sec секунд")
+        println("Неверный код. Ваш новый код авторизации: $correctCode")
+    }
 }
