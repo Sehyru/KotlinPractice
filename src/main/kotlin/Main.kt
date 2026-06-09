86330b210717a72996933da2118597656bb994fc
Sehyru <yakovenko1994@gmail.com>
2026-06-09 14:31:56 +0300

    KS-7-3: add space

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Введите любое число: ")
     val inputNumber = readln().toInt()
 
-    for(n in 0..inputNumber step 2) {
+    for (n in 0..inputNumber step 2) {
         println(n)
     }
 }
