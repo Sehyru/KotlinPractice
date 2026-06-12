d85e2c67a8cc915a3e7c0ca10838e51c61a18f0c
Sehyru <yakovenko1994@gmail.com>
2026-06-12 22:19:30 +0300

    KS-7-5: foreachindexed — difficult password

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
 package org.example
 
 fun main() {
+    val digits = '0'..'9'
+    val letters = 'a'..'z'
+    val uppercaseLetters = 'A'..'Z'
 
+    var passwordSize: Int
+
+    do {
+        print("Задайте длину пароля (минимум 6): ")
+        passwordSize = readln().toInt()
+
+        if (passwordSize < 6) println("Введите число не меньше 6.")
+    } while (passwordSize < 6)
+
+    var password = ""
+
+    for (i in 1..passwordSize) {
+        password += when (i % 3) {
+            0 -> digits.random()
+            1 -> letters.random()
+            else -> uppercaseLetters.random()
+        }
+    }
+    println(password)
 }
