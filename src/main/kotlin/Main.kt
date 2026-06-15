de50f479b190a0eb84349ae559fb493589b2e8eb
Sehyru <yakovenko1994@gmail.com>
2026-06-16 01:38:27 +0300

    KS-8-1: access to elements - recipe

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
 
 fun main() {
+    val ingredients = arrayOf("Картофель", "Сосиски", "Помидор", "Твердый сыр", "Сметана", "Соль")
 
+    println("Напишите ингридиент который хотите найти: ")
+    val userRequest = readln()
+
+    var found = false
+
+    for (ingredient in ingredients) {
+        if (ingredient == userRequest) {
+            found = true
+            break
+        }
+    }
+    if (found) println("Ингредиент $userRequest в рецепте есть")
+    else println("Такого ингредиента в рецепте нет")
 }
