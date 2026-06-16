da635e9db2e4660824096643674ba8c83e0597de
Sehyru <yakovenko1994@gmail.com>
2026-06-16 21:54:30 +0300

    KS-8-3: search in array - a recipe without a loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,9 @@ package org.example
 fun main() {
     val ingredients = arrayOf("Картофель", "Сосиски", "Помидор", "Твердый сыр", "Сметана", "Соль")
 
-    println("Напишите ингридиент который хотите найти: ")
+    println("Напишите ингредиент который хотите найти: ")
     val userRequest = readln()
 
-    var found = false
-
-    for (ingredient in ingredients) {
-        if (ingredient == userRequest) {
-            found = true
-            break
-        }
-    }
-    if (found) println("Ингредиент $userRequest в рецепте есть")
+    if (ingredients.contains(userRequest)) println("Ингредиент $userRequest в рецепте есть")
     else println("Такого ингредиента в рецепте нет")
 }
