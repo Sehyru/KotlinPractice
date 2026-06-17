69590a1853de171ef23b639392054eface5aa725
Sehyru <yakovenko1994@gmail.com>
2026-06-17 23:42:18 +0300

    KS-8-4: intarray - list ingredients

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
 
 fun main() {
+    val ingredients = arrayOf("Картофель", "Сосиски", "Помидор", "Твердый сыр", "Сметана", "Соль")
 
+    println("Список ингредиентов: ${ingredients.joinToString()}")
+    println("Выберите ингредиент который хотите заменить: ")
+    val selectedIngredient = readln()
+
+    if (ingredients.contains(selectedIngredient)) {
+        println("Добавьте новый ингредиент вместо ранее выбранного: ")
+        val addedIngredient = readln()
+
+        val index = ingredients.indexOf(selectedIngredient)
+        ingredients[index] = addedIngredient
+        println("Готово! Вы сохранили следующий список ингредиентов: ${ingredients.joinToString()}")
+    }
+    else println("Такого ингредиента в списке нет")
 }
