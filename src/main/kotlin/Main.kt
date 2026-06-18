b7454f73b94ecf573f013ee3e6145faa2711eff2
Sehyru <yakovenko1994@gmail.com>
2026-06-18 22:10:40 +0300

    KS-9-1: listof - ingredients list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
 fun main() {
-
+    val ingredients = listOf("Мука", "Яйца", "Молоко")
+    println("В рецепте есть следующие ингредиенты: $ingredients")
+    for (ingredient in ingredients) {
+        println(ingredient)
+    }
 }
