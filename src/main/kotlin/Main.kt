1461ad3f96a89cd178a05b231c4d297bb5b8d605
Sehyru <yakovenko1994@gmail.com>
2026-06-20 15:45:53 +0300

    KS-9-3: += — calculation of portions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package org.example
 
 fun main() {
+    val ingredients = listOf("Яйца", "Молоко", "Сливочное масло")
+    val count = listOf(2, 50, 15)
 
+    println("Введите количество порций: ")
+    val userInput = readln().toInt()
+
+    println(
+        "На $userInput порций вам понадобиться: ${
+            ingredients.mapIndexed { index, string ->
+                "$string-${count[index] * userInput}"
+            }.joinToString(", ")
+        }"
+    )
 }
