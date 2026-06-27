cc1a99a29f8d464a128ac5f0091c62aa51aecff9
Sehyru <yakovenko1994@gmail.com>
2026-06-27 22:46:30 +0300

    KS-9-4: find and filter - ingredient search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.example
 
 fun main() {
-
+    println("Напишите пять названий ингредиентов для блюда в одной строке через запятую с пробелом:")
+    val ingredients = readln().split(", ")
+    val sortedIngredients = ingredients.sorted()
+    println(sortedIngredients)
 }
