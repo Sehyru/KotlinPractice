1831d5396b011e2a5cec9d1a1dd23a23a1a1431a
Sehyru <yakovenko1994@gmail.com>
2026-06-28 03:32:21 +0300

    KS-9-5: sorted and map - sorting the list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package org.example
 
 fun main() {
+    println("Составим список ингредиентов для блюда. Введите 5 уникальных ингредиентов.")
+    val ingredients = mutableListOf<String>()
 
+    while (ingredients.size < 5) {
+        println("Ингредиент ${ingredients.size + 1} из 5:")
+        val ingredient = readln()
+        if (ingredients.contains(ingredient)) {
+            println("«$ingredient» уже в списке. Введите другой ингредиент:")
+        } else ingredients.add(ingredient)
+    }
+
+    val sortedIngredients = ingredients.sorted()
+    val result = sortedIngredients.joinToString(", ")
+    println("Ваши ингредиенты: ${result.replaceFirstChar { it.uppercase() }}")
 }
