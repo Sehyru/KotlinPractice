ffaf2efacb67d5ce64ee87bffad33195cda4ef14
Sehyru <yakovenko1994@gmail.com>
2026-07-05 17:37:24 +0300

    KS-10-4: named arguments - improved roll dice

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,42 @@
 package org.example
 
-fun main() {
+fun rollDice(): Int {
+    return (1..6).random()
+}
 
+fun playRound(): Boolean {
+    val rollPlayer = rollDice()
+    println("Ход игрока: выпало $rollPlayer")
+
+    val rollComputer = rollDice()
+    println("Ход компьютера: выпало: $rollComputer")
+
+    return when {
+        rollPlayer > rollComputer -> {
+            println("Победило человечество")
+            true
+        }
+
+        rollPlayer < rollComputer -> {
+            println("Победила машина")
+            false
+        }
+
+        else -> {
+            println("Победила дружба")
+            false
+        }
+    }
+
+}
+
+fun main() {
+    var wins = 0
+    do {
+        val playerWon = playRound()
+        if (playerWon) wins++
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        val userAnswer = readln()
+    } while (userAnswer == "Да")
+    println("Игрок выиграл партий: $wins")
 }
