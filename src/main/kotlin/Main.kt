1525c64f84d999c352220ab22fb9eeedc18ef835
Sehyru <yakovenko1994@gmail.com>
2026-07-07 14:05:13 +0300

    KS-10-5 fix null logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,8 @@ package org.example
 const val CREDENTIALS_LOGIN = "Sergey"
 const val CREDENTIALS_PASSWORD = "Bubbles"
 
-fun validateLogin(login: String, password: String): Boolean = login == CREDENTIALS_LOGIN && password == CREDENTIALS_PASSWORD
+fun validateLogin(login: String, password: String): Boolean =
+    login == CREDENTIALS_LOGIN && password == CREDENTIALS_PASSWORD
 
 fun generateToken(length: Int = 32): String {
     val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -12,17 +13,13 @@ fun generateToken(length: Int = 32): String {
 
 fun authorize(login: String, password: String): String? = if (validateLogin(login, password)) generateToken() else null
 
-fun getCart(token: String?): List<String>? {
-    val cartList = listOf("Компьютер", "Фотоаппарат", "Планшет")
-    return if (token != null) cartList else null
-}
+fun getCart(token: String): List<String> = listOf("Компьютер", "Фотоаппарат", "Планшет")
 
 fun main() {
     val token = authorize(CREDENTIALS_LOGIN, CREDENTIALS_PASSWORD)
-    val cart = getCart(token)
 
-    if (cart != null) {
-        println("Содержимое корзины: $cart")
+    if (token != null) {
+        println("Содержимое корзины: ${getCart(token)}")
     } else {
         println("Авторизация прошла неудачно!")
     }
