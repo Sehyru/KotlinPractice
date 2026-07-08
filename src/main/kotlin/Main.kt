ad83909db70880cab33582549f39ef2f83d5e3b3
Sehyru <yakovenko1994@gmail.com>
2026-07-08 23:11:02 +0300

    KS-11-2 properties — user profile

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,42 @@
 package org.example
 
-class User {
+class User2 {
     var id: Int = 0
     var login: String = ""
     var password: String = ""
     var email: String = ""
+    var bio: String = ""
+    fun info() {
+        println("#$id, $login, $password, $email, $bio")
+    }
+
+    fun updateBio() {
+        println("Заполни bio: ")
+        bio = readln()
+    }
+
+    fun changePassword() {
+        println("Изменение пароля. Введите текущий пароль: ")
+        val currentPassword = readln()
+        if (currentPassword == password) {
+            println("Введите новый пароль: ")
+            password = readln()
+            println("Пароль изменен.")
+        } else {
+            println("Неверный текущий пароль.")
+        }
+    }
 }
 
 fun main() {
-    val user1 = User()
-    val user2 = User()
+    val user = User2()
+    user.id = 1
+    user.login = "user1"
+    user.password = "pass1"
+    user.email = "user1@gmail.com"
 
-    user1.id = 1
-    user1.login = "user1"
-    user1.password = "pass1"
-    user1.email = "user1@gmail.com"
-
-    user2.id = 2
-    user2.login = "user2"
-    user2.password = "pass2"
-    user2.email = "user2@gmail.com"
-
-    println("#${user1.id}, ${user1.login}, ${user1.password}, ${user1.email}")
-    println("#${user2.id}, ${user2.login}, ${user2.password}, ${user2.email}")
+    user.info()
+    user.updateBio()
+    user.changePassword()
+    user.info()
 }
