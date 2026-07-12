aadfe4746ebf975ab12c1494053ad05f4f32ae84
Sehyru <yakovenko1994@gmail.com>
2026-07-12 03:05:14 +0300

    KS-12-1 primary constructor - weather

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package org.example
 
-fun main() {
+class DayWeather {
+    var dayTemp: Int = 20
+    var nightTemp: Int = 12
+    var precipitation: Boolean = false
+    fun printInfo() {
+        val precipitationText = if (precipitation) "да" else "нет"
+        println("Температура днем: $dayTemp, температура ночью: $nightTemp, осадки: $precipitationText")
+    }
+}
 
+fun main() {
+    val day1 = DayWeather()
+    val day2 = DayWeather()
+
+    day1.dayTemp = 24
+    day1.nightTemp = 14
+    day1.precipitation = true
+    day1.printInfo()
+
+    day2.dayTemp = 18
+    day2.nightTemp = 9
+    day2.precipitation = true
+    day2.printInfo()
 }
