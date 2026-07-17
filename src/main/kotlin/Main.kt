646d2657735475e727afb617213412b80ffe07fa
Sehyru <yakovenko1994@gmail.com>
2026-07-17 19:37:33 +0300

    KS-13-2 operator ? - safe call

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.example
 
-fun main() {
+class Info(val name: String, val phone: Long, val company: String? = null) {
+    fun printInfo() {
+        println("Имя: $name\nНомер: $phone\nКомпания: ${company ?: "не указано"}")
+    }
+}
 
+fun main() {
+    val info = Info("Ростислав", 89123456789, "Reddit")
+    info.printInfo()
 }
