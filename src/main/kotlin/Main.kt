66af8db20ff371d188895c504c4dd1bac8aa05df
Sehyru <yakovenko1994@gmail.com>
2026-07-18 20:56:31 +0300

    KS-13-3 Elvis ?: - default value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
 package org.example
 
-fun main() {
+class Contact(val name: String, val phone: Long, val company: String? = null)
 
+fun main() {
+    val contacts = listOf(
+        Contact("Ростислав", 89123456789, "Reddit"),
+        Contact("Анна", 89234567891, null),
+        Contact("Игорь", 89345678912, "null"),
+        Contact("Мария", 89456789123, null),
+        Contact("Дмитрий", 89567891234, "Google")
+    )
+
+    val companies = mutableListOf<String>()
+    for (contact in contacts) {
+        val company = contact.company ?: "<не указано>"
+        if (!companies.contains(company)) {
+            companies.add(company)
+        }
+    }
+
+    println(companies)
 }
