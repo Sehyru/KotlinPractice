0d1528c341d5e82ee9101654325c794dea9f97b9
Sehyru <yakovenko1994@gmail.com>
2026-07-20 00:20:53 +0300

    KS-13-4 operator !! - phonebook from console

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,37 @@
 package org.example
 
-fun main() {
+class Contact(val name: String, val phone: Long, val company: String? = null) {
+    fun printInfo() {
+        println("Имя: $name\nНомер: $phone\nКомпания: ${company ?: "не указано"}")
+    }
+}
 
+fun main() {
+    val contacts = mutableListOf<Contact>()
+
+    println("Сколько котактов хотите добавить?")
+    val count = readln().toInt()
+
+    while (contacts.size < count) {
+        println("Введите имя:")
+        val name = readln()
+
+        println("Введите номер телефона:")
+        val phoneInput = readln().toLongOrNull()
+
+        if (phoneInput == null) {
+            println("Номер телефона не введён, контакт не будет добавлен")
+            continue
+        }
+
+        println("Введите компанию (можно оставить пустым):")
+        val companyInput = readln()
+        val company = if (companyInput.isBlank()) null else companyInput
+
+        contacts.add(Contact(name, phoneInput, company))
+    }
+
+    for (contact in contacts) {
+        contact.printInfo()
+    }
 }
