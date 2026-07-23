6a4c0a163b0bd1da7870fd57f6a523d06dbee7b2
Sehyru <yakovenko1994@gmail.com>
2026-07-23 19:38:29 +0300

    KS-13-5 let - debug

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.example
 
-fun main() {
+class ContactError(val name: String, val phone: Long)
 
+fun main() {
+    println("Введите номер телефона: ")
+    val input = readln()
+
+    input.toLongOrNull()?.let {
+        println("Номер телефона: $it")
+    } ?: println("Ошибка: введено не число")
 }
